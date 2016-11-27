208d73dc7a69ec02742dd4703ceb482879be1805
ballabalu <ebarig@gmail.com>
2016-11-27 11:56:23 +0100

    die Klasse Ball in der initialen Version erstellt; Bibliotheken sind noch nicht optimal importiert, scheint aber ein Problem auf meinem Rechner zu sein

--- pong_game/src/model/Ball.java
+++ pong_game/src/model/Ball.java
@@ -1,5 +1,24 @@
 package model;
+import java.awt.Component;
+import java.awt.Graphics;
 
 public class Ball {
-
+	private int x, y;
+	private Component comp;
+	public Ball(int x, int y, Component comp){
+		this.x = x;
+		this.y = y;
+		this.comp = comp;
+	}
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public void paint(Graphics gr){
+		gr.drawImage(Toolkit.getDefaultToolkit.,x, y, comp);
+	}
 }
