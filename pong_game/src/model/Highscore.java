9beac4fb81a4790bc3e23f9013f3c9398e67afe9
/n <erdbeersche.git@gmail.com>
2016-11-28 12:08:15 +0100

    Player, PlayerComparator, Highscore und HighscoreTester erstellt. Läuft aber noch nicht alles ;)

---
+++ pong_game/src/model/Highscore.java
@@ -0,0 +1,135 @@
+package model;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Highscore {
+
+	private ArrayList<Player> playerList;
+	
+	private static final String HIGHSCORE_FILE = "pongHighscore.txt";
+	
+	ObjectOutputStream outputStream = null;
+	ObjectInputStream inputStream = null;
+	
+	
+	public Highscore(){
+		playerList = new ArrayList<Player>();
+		
+	}
+	
+	public ArrayList<Player> getScores(){
+		System.out.println("getScores");
+		loadScoreFile();
+		sort();
+		return playerList;
+	}
+	
+	public void sort(){
+		System.out.println("sort");
+		PlayerComparator vergleich = new PlayerComparator();
+		Collections.sort(playerList, vergleich);
+		
+	}
+	
+	public void scoreHinzufügen(String name, int score){
+		System.out.println("scoreHinzufügen");
+		loadScoreFile();
+		playerList.add(new Player(name, score));
+		updateScoreFile();
+		
+	}
+	
+	private void loadScoreFile() {
+		System.out.println("loadScoreFile");
+		try{
+			inputStream = new ObjectInputStream(new FileInputStream(HIGHSCORE_FILE));
+			playerList = (ArrayList<Player>) inputStream.readObject();
+		}catch (FileNotFoundException e){
+			System.out.println("FILE NOT FOUND: )" + e.getMessage()) ;
+		}catch (IOException e){
+			System.out.println("IOException: )" + e.getMessage()) ;
+		}catch (ClassNotFoundException e){
+			System.out.println("ClassNotFoundException: )" + e.getMessage()) ;
+		}finally{
+			try {
+				if(outputStream != null){
+					outputStream.flush();
+					outputStream.close();
+				}
+			}catch (IOException e){
+				System.out.println("IO - ERROR: )" + e.getMessage()) ;
+			}
+		}
+		
+	}
+	private void updateScoreFile() {
+		System.out.println("updateScoreFile");
+		try{
+			outputStream = new ObjectOutputStream(new FileOutputStream(HIGHSCORE_FILE));
+			outputStream.writeObject(playerList);
+		}catch (FileNotFoundException e){
+			System.out.println("FILE NOT FOUND: )" + e.getMessage()) ;
+		}catch (IOException e){
+			System.out.println("IOException: )" + e.getMessage()) ;
+		}finally{
+			try {
+				if(outputStream != null){
+					outputStream.flush();
+					outputStream.close();
+				}
+			}catch (IOException e){
+				System.out.println("IO - ERROR: )" + e.getMessage()) ;
+			}
+		}
+		
+	}
+
+	public String getHighscoreString(){
+		System.out.println("getHighscoreString");
+		
+		
+		String highscoreString = "";
+		int maxScores = 10;
+		
+		
+		ArrayList<Player> player = null;
+		player = getScores();
+		
+		System.out.println("player = getScores(); " + player);
+		
+		int i = 0;
+		int size = player.size();
+		
+		if(size > maxScores){
+			size = maxScores;
+		}
+		
+		while (i < size){
+			System.out.println(player.get(i).getPlayerName() + " " + player.get(i).getScore());
+			highscoreString +=  (i+1) +  "." + player.get(i).getPlayerName() +  " " + player.get(i).getScore() + " Punkte ------ ";
+			i++;
+		}
+		return highscoreString;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ pong_game/src/model/HighscoreTester.java
@@ -0,0 +1,62 @@
+package model;
+
+public class HighscoreTester {
+
+	
+	public static void main (String[] args){
+		Highscore highscore = new Highscore();
+		highscore.scoreHinzufügen("aaa", 200);
+		highscore.scoreHinzufügen("bbb", 300);
+		highscore.scoreHinzufügen("ccc", 300);
+		highscore.scoreHinzufügen("ddd", 1000);
+		highscore.scoreHinzufügen("eee", 100);
+		highscore.scoreHinzufügen("fff", 1200);
+		
+		System.out.print(highscore.getHighscoreString());
+		
+	}
+	
+	
+	/*
+	public static void main (String[] args){
+		System.out.println("ScoreTester");
+		
+		Score_alt globalScore = new Score_alt();
+		
+		Player player1 = new Player(1, "aaaa");
+		Player player2 = new Player(2, "bbbb");
+		
+		System.out.println(player1.getPlayerName() + ": " + player1.getPlayerScore());
+		
+		System.out.println("player1.increasePlayerScore()");
+		player1.increasePlayerScore();
+		System.out.println(player1.getPlayerName() + ": " + player1.getPlayerScore());
+		
+		System.out.println();
+		System.out.println("globalScore.printScore();");
+		globalScore.printScore();
+		
+		System.out.println();
+		System.out.println("globalScore.addPlayer();");
+		globalScore.addPlayer(player1);
+		globalScore.addPlayer(player2);
+		globalScore.printScore();
+		
+		System.out.println();
+		System.out.println("player1.increasePlayerScore() + player2.increasePlayerScore()");
+		player1.increasePlayerScore();
+		player2.increasePlayerScore();
+		globalScore.printScore();
+		
+		System.out.println();
+		System.out.println("getWinner()");
+		Player player3 = new Player(1, "cccc");
+		Player player4 = new Player(2, "dddd");
+		globalScore.addPlayer(player3);
+		globalScore.addPlayer(player4);
+		globalScore.printScore();
+		System.out.println("Winner: " + globalScore.getWinner().getPlayerName());
+	}
+	
+	*/
+}
