d84fa023deb6352b4a99642b7fcf04d20b7ee273
affe <affe@affe-PC.fritz.box>
2016-12-08 21:00:13 +0100

    MenueView erstellt. Controller enthät ActionListeneer für die Btn Clicks

--- pong_game/src/controller/PongTest.java
+++ pong_game/src/controller/PongTest.java
@@ -7,13 +7,14 @@ import java.awt.Toolkit;
 import javax.swing.*;
 
 import model.Pitch;
+import view.MenueView;
 import view.PongView;
 
 public class PongTest extends JFrame{
 		
 	public static void main(String[] args){
-		PongView pong = new PongView("Pong - The Game");
-		pong.init();
+		MenueView pong = new MenueView("Pong - The Game - Test");
+		pong.init(new Controller());
 
 	}
    
