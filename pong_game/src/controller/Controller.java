cdefd5cf7306c028c53ac699d9e65039fa63f8fa
ballabalu <ebarig@gmail.com>
2016-12-12 21:27:57 +0100

    Stevens Version wiederhergestellt

--- pong_game/src/controller/Controller.java
+++ pong_game/src/controller/Controller.java
@@ -1,29 +1,43 @@
 package controller;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import view.MenueView;
+import model.Pitch;
 import view.PongView;
 
+public class Controller {
 
-public class Controller implements ActionListener {
+    /*private Pitch pitch;
+    private PongView pongview;
+    private ActionListener actionListener;
+    
+    public Controller(Pitch pitch, PongView pongview){
+        this.pitch = pitch;
+        this.pongview = pongview;
+                          
+    }
+    
+    public void control(){                     
+    	pongview.getBtnRedraw().addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {                  
+          	  pongview.getRechti().setColor(pitch.getColor());
+          	  pongview.getRechti().setSize(pitch.getWidth(), pitch.getHeight());
+            }
+    	});
+                     
+        pongview.changeColor().addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {                  
+          	  if (pitch.getColor() == Color.blue){
+          		pitch.setColor(Color.red);
+          	  } else {
+          		pitch.setColor(Color.blue); 
+          	  }
+            }
+        }); 
+    }*/
+    
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getActionCommand() == "Spiel starten"){
-			PongView pong = new PongView("Pong - The Game");
-			pong.init();
-			MenueView.pong.dispose();
-        }		
-        else if(e.getActionCommand() == "Highscores"){
-        	System.out.println("highscore");
-        }
-        else if(e.getActionCommand() == "Spiel beenden"){
-        	System.exit(0);
-        }
-		
-	}
- 
 	
 }
--- pong_game/src/controller/PongTest.java
+++ pong_game/src/controller/PongTest.java
@@ -12,12 +12,16 @@ import javax.swing.*;
 import model.Ball;
 import model.Paddle;
 import model.Pitch;
-import view.MenueView;
 import view.PongView;
 
 public class PongTest extends JFrame{
 	
 		
+
+	public static void main(String[] args){
+		PongView pong = new PongView("Pong - The Game");
+		pong.init();
+
 
 
 
