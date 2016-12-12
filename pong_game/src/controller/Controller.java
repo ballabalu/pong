85370f53265263a50938b4f119d47a8a3b0bbabe
/n <erdbeersche.git@gmail.com>
2016-12-12 21:58:00 +0100

    menueview und controller

--- pong_game/src/controller/Controller.java
+++ pong_game/src/controller/Controller.java
@@ -1,5 +1,37 @@
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import view.MenueView;
+import view.PongView;
+
+
+public class Controller implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand() == "Spiel starten"){
+			PongView pong = new PongView("Pong - The Game");
+			pong.init();
+			MenueView.pong.dispose();
+        }		
+        else if(e.getActionCommand() == "Highscores"){
+        	System.out.println("highscore");
+        }
+        else if(e.getActionCommand() == "Spiel beenden"){
+        	System.exit(0);
+        }
+		
+	}
+ 
+	
+}
+
+ /*
+ 
+package controller;
+
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,6 +41,8 @@ import view.PongView;
 
 public class Controller {
 
+	
+	
     /*private Pitch pitch;
     private PongView pongview;
     private ActionListener actionListener;
@@ -40,4 +74,4 @@ public class Controller {
     
 
 	
-}
+
---
+++ pong_game/src/view/MenueView.java
@@ -0,0 +1,64 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+
+public class MenueView extends JFrame{
+
+	private Graphics gr;
+	private JButton newGame, highscore, exitGame;
+	public static MenueView pong;
+	
+	public MenueView(String title){
+		super(title);
+	}	
+
+	public void init(ActionListener listener){
+		pong = new MenueView("Pong - The Game - Menü");
+		pong.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+        JPanel contentPane = new JPanel();
+        contentPane.setOpaque(true);
+        contentPane.setBackground(Color.WHITE);
+        contentPane.setLayout(null);       
+
+        newGame = new JButton("Spiel starten");
+        highscore = new JButton("Highscores");
+        exitGame = new JButton("Spiel beenden");
+               
+        newGame.setBounds(350, 345, 200, 40);
+        highscore.setBounds(350, 395, 200, 40);
+        exitGame.setBounds(350, 445, 200, 40);
+        
+		newGame.setBackground(Color.WHITE);
+		newGame.setFont(new Font("Arial", Font.PLAIN, 18));
+		  
+		highscore.setBackground(Color.WHITE);
+		highscore.setFont(new Font("Arial", Font.PLAIN, 18));
+		  
+		exitGame.setBackground(Color.WHITE);
+		exitGame.setFont(new Font("Arial", Font.PLAIN, 18));
+        
+		newGame.addActionListener(listener);
+		highscore.addActionListener(listener);
+		exitGame.addActionListener(listener);
+		
+		contentPane.add(newGame);
+		contentPane.add(highscore);
+		contentPane.add(exitGame);
+				
+		pong.setContentPane(contentPane);
+		pong.setSize(900, 600);
+		pong.setLocation(50,50);
+		pong.setResizable(false);
+		pong.setVisible(true);
+
+	}
+
+}
