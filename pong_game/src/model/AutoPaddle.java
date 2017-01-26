b6097e50239faf1ea491288c6c1fe61abef46ccd
affe <affe@affe-PC.fritz.box>
2017-01-26 19:55:01 +0100

    Paddle Kommentare und AutoPaddel Kommentare ergänzt

--- pong_game/src/model/AutoPaddle.java
+++ pong_game/src/model/AutoPaddle.java
@@ -1,20 +1,19 @@
-/**
- * AutoPaddle ist das KI-Paddle
- * 
- * @author Enrico Barig, Steven Kranhold, Naamah Richter, Stefanie Schwanke
- * @version 1.0, Stand: 17/01/26
- * 
- */
-
 package model;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+/**
+ * AutoPaddle.java beinhaltet Atrribute und Methoden die
+ * für das KI-Paddel nötig sind.
+ * 
+ * @author Enrico Barig, Steven Kranhold, Naamah Richter, Stefanie Schwanke
+ * @version 1.0, Stand: 17/01/26
+ * 
+ */
 public class AutoPaddle {
 	
-
 	private int x;
 	private int y;
 	private int width;
@@ -23,11 +22,11 @@ public class AutoPaddle {
 	
 	/**
 	 * Konstruktor der Klasse AutoPaddle
-	 * @param x
-	 * @param y
-	 * @param width
-	 * @param height
-	 * @param color
+	 * @param x x-Koordinate des AutoPaddels
+	 * @param y y-Koordinate des AutoPaddels
+	 * @param width Breite des AutoPaddels
+	 * @param height Höhe des AutoPaddels
+	 * @param color Farbe des AutoPaddels
 	 */
 	public AutoPaddle (int x, int y, int width, int height, Color color){
 		this.x = x;
@@ -56,7 +55,7 @@ public class AutoPaddle {
 	/**
 	 * Methode, die Ã¼berprÃ¼ft, ob sich Ball und AutoPaddle beruehren.
 	 * Ist dies der Fall, wir die collideWithPaddle-Methode aus der Klasse Ball aufgerufen
-	 * @param ball
+	 * @param ball Das Ball-Objekt wird übergeben
 	 */
 	public void checkCollisionWithBall(Ball ball){
 		Rectangle ballRect = new Rectangle(ball.getX(), ball.getY(), 50,50);
@@ -69,7 +68,7 @@ public class AutoPaddle {
 	
 	/**
 	 * Methode, die fÃ¼r die Bewegung des AutoPaddle verantwortlich ist.
-	 * @param ball
+	 * @param ball Das Ball-Objekt wird übergeben
 	 */
 	public void moveAutoPaddle(Ball ball){
 		
@@ -87,32 +86,40 @@ public class AutoPaddle {
 	
 	
 	/**
-	 * Getter und Setter fuer x
-	 * @return
+	 * Gibt die x-Koordinate des AutoPaddels zurück.
+	 * @return x x-Koordinate des AutoPaddels
 	 */
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * Setzt die x-Koordinate des AutoPaddels.
+	 * @param x x-Koordinate des AutoPaddels
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 
 	/**
-	 * Getter und Setter fuer y
-	 * @return
+	 * Gibt die y-Koordinate des AutoPaddels zurück.
+	 * @return y y-Koordinate des AutoPaddels
 	 */
 	public int getY() {
 		return y;
 	}
 
+	/**
+	 * Setzt die y-Koordinate des AutoPaddels.
+	 * @param y y-Koordinate des AutoPaddels
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 
 	/**
 	 * Paintt-Methode ist fÃ¼r die optische Erscheinung des AutoPaddle verantwortlich.
-	 * @return
+	 * @param gr Grafikobjekt
 	 */
 	public void paintt(Graphics gr){
 		gr.fillRect(x, y, width, height);
--- pong_game/src/model/Paddle.java
+++ pong_game/src/model/Paddle.java
@@ -5,16 +5,31 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.Observable;
 
+/**
+ * Paddle.java beinhaltet Atrribute und Methoden die
+ * für das Spieler-Paddel nötig sind.
+ * 
+ * @author Enrico Barig, Steven Kranhold, Naamah Richter, Stefanie Schwanke
+ * @version 1.0, Stand: 17/01/26
+ * 
+ */
 public class Paddle extends Observable{
 	
-	//Variablen
 	private int x;
 	private int y;
 	private int width;
 	private int height;
 	private Color color;
 	
-	//Konstruktor
+	/**
+	 * Konstruktor der Paddle-Klasse
+	 * Paddle wird hinsichtlich seiner Maße, Platzierung und Farbe initialisiert.
+	 * @param x x-Koordinate des Paddels
+	 * @param y y-Koordinate des Paddels
+	 * @param width Breite des Paddels
+	 * @param height Höhe des Paddels
+	 * @param color Farbe des Paddels
+	 */
 	public Paddle (int x, int y, int width, int height, Color color){
 		
 		this.x = x;
@@ -25,18 +40,25 @@ public class Paddle extends Observable{
 		
 	}
 	
+	/**
+	 * Methode die das Paddle nach oben bewegt.
+	 */
 	public void moveUp(){
-		//y--;
 		y -= 10;
-		//ggf. y- = 5, 7 oder 10 oder so
 	}
 	
+	/**
+	 * Methode die das Paddle nach unten bewegt.
+	 */
 	public void moveDown(){
-		//y++;
 		y += 10;
-		// hier vielleicht auch die Zahl erhÃ¶hen fÃ¼r eine schnellere Bewegung
 	}
 	
+	/**
+	 * Methode die überprüft ob der Ball mit dem
+	 * Spielerpaddle kollidiert.
+	 * @param ball Das Ball-Objekt wird übergeben
+	 */
 	public void checkCollisionWithBall(Ball ball){
 		Rectangle ballRect = new Rectangle(ball.getX(), ball.getY(), 50,50);
 		Rectangle paddleRect = new Rectangle(this.x, this.y, this.width, this.height);
@@ -45,28 +67,46 @@ public class Paddle extends Observable{
 			ball.collideWithPaddle(true);
 			//Observer informieren und 1 uebergeben
 			setChanged();
-			notifyObservers(1);
-			
+			notifyObservers(1);		
 		}
 	}
 	
+	/**
+	 * Gibt die x-Koordinate des Paddels zurück.
+	 * @return x x-Koordinate des Paddels
+	 */
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * Setzt die x-Koordinate des Paddels.
+	 * @param x x-Koordinate des Paddels
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 	
+	/**
+	 * Gibt die y-Koordinate des Paddels zurück.
+	 * @return y y-Koordinate des Paddels
+	 */
 	public int getY() {
 		return y;
 	}
 
+	/**
+	 * Setzt die y-Koordinate des Paddels.
+	 * @param y y-Koordinate des Paddels
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 
-	//paintt
+	/**
+	 * Methode die das Paddel zeichnet.
+	 * @param gr Grafikobjekt
+	 */
 	public void paintt(Graphics gr){
 		gr.fillRect(x, y, width, height);
 		gr.setColor(color);
